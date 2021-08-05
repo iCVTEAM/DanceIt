bfc28dcdf931f4cfd82b7ca59e1c9d0ada858241
Xin Guo <43775578+aszxnm@users.noreply.github.com>
2021-08-05 10:33:22 +0800

    Update graph.py

--- utils/graph.py
+++ utils/graph.py
@@ -40,14 +40,17 @@ class Graph():
 
     def get_edge(self, layout):
         if layout == 'openpose':
-            self.num_node = 23
+            self.num_node = 18
             self_link = [(i, i) for i in range(self.num_node)]
-            '''
             neighbor_link = [(4, 3), (3, 2), (7, 6), (6, 5), (13, 12), (12,
                                                                         11),
                              (10, 9), (9, 8), (11, 5), (8, 2), (5, 1), (2, 1),
                              (0, 1), (15, 0), (14, 0), (17, 15), (16, 14)]
-            '''
+            self.edge = self_link + neighbor_link
+            self.center = 1
+        elif layout == 'tsinghua':
+            self.num_node = 23
+            self_link = [(i, i) for i in range(self.num_node)]
             neighbor_link = [(0, 1), (3, 4), (4, 5), (5, 6), (2, 7), (7, 8),
                              (8, 9), (10, 11), (12, 13), (13, 14), (14, 15),
                              (2, 16), (16, 17), (17, 18), (19, 20), (3, 12),
